cf6ffe9eab1db95f9469386ec773182b94843ba9
Dima Ryabinin <sputnikcmd@gmail.com>
2019-12-07 14:19:41 +0300

    add: add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = () => {
+    return (
+    <div className='header'>
+      <div className="container">
+        <div className="header__title">network</div>
+      </div>
+    </div>
+    )
+  };
+
+export default Header;
