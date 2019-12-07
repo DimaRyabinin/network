03862d56278003cbded690263f2b347cfa9c13e2
Dima Ryabinin <sputnikcmd@gmail.com>
2019-12-07 14:21:08 +0300

    add: add Profile.jsx

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Profile() {
+    return (
+        <div className='content'>
+            <img className='content__img' src='https://yurf.pskgu.ru/app/pskgu/layouts/images/nophoto.png' />
+        </div>
+    )
+}
+
+export default Profile;
